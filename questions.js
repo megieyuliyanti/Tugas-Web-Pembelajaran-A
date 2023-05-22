680da60c13941eee42033f5293f22b031b8fe993
megieyuliyanti <126136032+megieyuliyanti@users.noreply.github.com>
2023-05-22 10:48:21 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Apa yang dimaksud dengan busana anak?",
+    answer: "Salah satu busana yang dipakai oleh anak-anak berumur 1 sampai 12 tahun.",
+    options: [
+      "Mempunyai kantung (saku) yang dapat dipakai untuk menyimpan sesuatu",
+      "Salah satu busana yang dipakai oleh anak-anak berumur 1 sampai 12 tahun.",
+      "Busana cenderung agak longgar sehingga mudah dan leluasa untuk bergerak",
+      "Mudah mengenakan dan membukanya"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Klasifikasi busana anak berdasarkan kesempatan, kecuali?",
+    answer: "Laki-laki dan Perempuan",
+    options: [
+      "Laki-laki dan Perempuan",
+      "Busana Sekolah dan Busana Pesta",
+      "Busana Rekreasi",
+      "Busana Bermain"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Kemeja mempunyai desain dasar yaitu ...",
+    answer: "kerah kemeja yang ada penegaknya, lengan licin, dan manset lengan",
+    options: [
+      "saku dalam dari bagian sisi, kupnat, dibagian belakang",
+      "salah satu busana anak berupa atasan yang dikenakan anak laki-laki",
+      "lapisan pireggang atau menggunakan karet elastik pada bagian belakang",
+      "kerah kemeja yang ada penegaknya, lengan licin, dan manset lengan"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Karakteristik dari busana anak antara lain ...",
+    answer: "cenderung agak longgar sehingga mudah dan leluasa untuk bergerak",
+    options: [
+      "memiliki lapisan pireggang atau menggunakan karet elastik pada bagian belakang",
+      "cenderung agak longgar sehingga mudah dan leluasa untuk bergerak",
+      "terdiri dari 2 jenis saja yaitu busana bermain dan busana sekolah.",
+      "terdiri dari 4 jenis yaitu busana pesta, busana tidur, busana sekolah, dan busana bermain."
+    ]
+  },
+    {
+    numb: 5,
+    question: "Kemeja anak laki-laki merupakan ....",
+    answer: "salah satu busana anak berupa atasan yang dikenakan anak laki-laki",
+    options: [
+      "salah satu busana yang dipakai oleh anak-anak berumur 1 sampai 12 tahun",
+      "busana cenderung agak longgar sehingga mudah dan leluasa untuk bergerak ",
+      "salah satu busana anak berupa atasan yang dikenakan anak laki-laki",
+      "dengan kerah kemeja yang ada penegaknya, lengan licin, dan manset lengan pada lengan panjang"
+    ]
+  },
+];
